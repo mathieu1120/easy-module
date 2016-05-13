606dcc0ec58b44768ab6336bef1ff86479688999
mathieu <mathieu@gofundme.com>
2016-05-12 22:16:52 -0700

    make a table out of the products

--- etsy.php
+++ etsy.php
@@ -39,11 +39,23 @@ class Etsy extends Module {
 //when cliicking on the row, show the option to remove the product or to add the product
         $etsy = new EtsyAPI('0f9qw3ig8eis8gsh09cb9gzq');
         $products = $etsy->getEtsyProduct();
-        d($products);
-        return 'lol';
+        $html = '<table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>created at</th>
+                    </tr>';
+        foreach ($products as $product) {
+            $html .= '<tr>
+                        <th>'.$product->listing_id.'</th>
+                        <th>'.$product->title.'</th>
+                        <th>'.$product->price.'</th>
+                        <th>'.date('Y-m-d H:i:s', strtotime($product->creation_tsz)).'</th>
+                    </tr>';
+        }
+        return $html.'</table';
     }
-
-
 }
 
 class EtsyAPI {
