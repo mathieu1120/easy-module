05c676c208f3216c2b6258c92efa3500b800f9fa
mathieu <mathieu@gofundme.com>
2016-05-15 21:09:07 -0700

    rewrite url

--- etsy.php
+++ etsy.php
@@ -83,7 +83,7 @@ class Etsy extends Module {
             $newProduct = new Product();
             $newProduct->name[(int)Configuration::get('PS_LANG_DEFAULT')] = $etsyProduct->title;
             $newProduct->price = $etsyProduct->price;
-            $newProduct->link_rewrite[(int)Configuration::get('PS_LANG_DEFAULT')] = strip_tags(html_entity_decode($etsyProduct->title));
+            $newProduct->link_rewrite[(int)Configuration::get('PS_LANG_DEFAULT')] = str_replace([' ', '/'], '_', strip_tags(html_entity_decode($etsyProduct->title));
             d($newProduct->link_rewrite);
             $newProduct->description[(int)Configuration::get('PS_LANG_DEFAULT')] = $etsyProduct->description;
             $newProduct->id_category_default = $parentCategoryPs;
