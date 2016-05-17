26ee6fa2eae054c113bc39872b98ec6d7f2c4b45
mathieu <mathieu@gofundme.com>
2016-05-16 21:51:19 -0700

    debug product

--- etsy.php
+++ etsy.php
@@ -122,7 +122,7 @@ class Etsy extends Module {
         $newProduct->name[(int)Configuration::get('PS_LANG_DEFAULT')] = $etsyProduct->title;
         $newProduct->price = $etsyProduct->price;
         $newProduct->link_rewrite[(int)Configuration::get('PS_LANG_DEFAULT')] = str_replace([' ', '/'], '_', strip_tags(html_entity_decode($etsyProduct->title)));
-        echo $etsyProduct->description;
+        echo nl2br($etsyProduct->description);
         die();
         $newProduct->description[(int)Configuration::get('PS_LANG_DEFAULT')] = $etsyProduct->description;
         $newProduct->id_category_default = $parentCategoryPs;
