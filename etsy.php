e11c271b04b5828c1f950aa76df4c394a7fecbc4
mathieu <mathieu@gofundme.com>
2016-05-16 22:34:51 -0700

    debug product

--- etsy.php
+++ etsy.php
@@ -172,6 +172,7 @@ class EtsyAPI {
 
     public function getEtsyProductImages($listingId) {
         $url = "https://openapi.etsy.com/v2/shops/ShopRachaels/listings/".$listingId."/images?api_key=".$this->api_string;
+        d($url);
         return $this->_curlMeThis($url);
     }
 }
